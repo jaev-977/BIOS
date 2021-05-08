9308da4b1b0b2edb5f819fa3fe9a66d004666f3a
diferpaho <46627032+diferpaho@users.noreply.github.com>
2021-05-07 20:11:16 -0500

    Update views.py

--- main/views.py
+++ main/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 
 def home(request):
     return HttpResponse('<h1>We are developing this site!</h1>'
-                        '<div>Project developed by Enciso Systems<a href="https://www.encisosystems.com/" target="_blank">Enciso Systems</a>.</div>')
+                        '<div>Project developed by <a href="https://www.encisosystems.com/" target="_blank">Enciso Systems</a>.</div>')
